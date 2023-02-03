20b27c18c317b515756499d161c89edb9ac1494b
luciano.vannelli <luciano.vannelli@ecloudsolutions.com>
2023-02-03 14:36:26 -0300

    f

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
+import cors from "cors";
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-const pool = new pg.Pool();
+// const pool = new pg.Pool();
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json());
@@ -14,8 +16,10 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  // const { rows } = await pool.query("SELECT NOW()");
+  // res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  res.send(`Hello, World! The time from the DB is`);
+
 });
 
 app.get("/api/lucho", async (req, res) => {
