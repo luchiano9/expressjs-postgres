e66d8cd1f2464b8f10841133f3d70879452536a1
luciano.vannelli <luciano.vannelli@ecloudsolutions.com>
2023-02-03 14:54:43 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get("/", async (req, res) => {
   const { rows } = await pool.query("SELECT NOW()");
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3333');
   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
 });
 
 app.get("/api/lucho", async (req, res) => {
   console.log("FUx");
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3333');
   res.send(`Hello, World! The time from the DB is`);
 });
 
