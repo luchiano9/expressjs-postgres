10b529fcf49c29269df137cf133e5f15c32c78c8
luciano.vannelli <luciano.vannelli@ecloudsolutions.com>
2023-02-06 10:55:42 -0300

    options"

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,13 @@ app.use(bodyParser.text({ type: "text/html" }));
 // Permitir cualquier origen
 app.use(cors({ origin: true }))
 
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.send()
+})
+
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
