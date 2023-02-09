7afc944d8d574be0e01b28168eaddcf9f070ec68
luciano.vannelli <luciano.vannelli@ecloudsolutions.com>
2023-02-09 16:33:28 -0300

    3

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,7 @@ app.use(cors(),(req,res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   res.setHeader('Content-Type', 'application/json');
-  let mensaje = req.body;
-    res.send(JSON.stringify({ message: 'Hello World!' , message2: mensaje }));
+  res.send(JSON.stringify({ message: 'Hello World!' }));
     
 });
   
