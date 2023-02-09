58f4f5f67441d03513142254522a8c8476405423
luciano.vannelli <luciano.vannelli@ecloudsolutions.com>
2023-02-09 16:22:58 -0300

    otrotry

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ app.use(cors(),(req,res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ message: 'Hello World!' }))
+    
 });
   
 const port = process.env.PORT || 3333;
